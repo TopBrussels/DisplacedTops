4ad9a24fc9665bf5d69f7f8057c3eeece0563565
quentin <quentin.python@vub.ac.be>
2015-03-05 14:37:06 +0100

    adds README, adds a variable to access the Ntuple when plotting

--- scripts/stackplot.py
+++ scripts/stackplot.py
@@ -54,19 +54,21 @@ samplesaddtoleg=[1,1,1,0,1,0,0,0,0,0,0,0,0]
 samples = ["dy","ttbar","wjetsplus","wjetsminus","qcd1","qcd2","qcd3","qcd4","qcd5","qcd6","qcd7","qcd8","qcd9"]
 samplesxsecs = [2008.4,831.76,11811.4,8677.3,677300000.*0.007,866600000.*0.00044,164300000.*0.00816,21810000.*0.01522,2999000.*0.02424,3529000.*0.158,128500.*0.0406,185900000.*0.00272,3495000.*0.01255]
 samplespresel = [2820473.0,25437856.0,699606.0,226439.0,1986513.0,4768929.0,3742583.0,3893676.0, 3468633.,1958930.0,999553.0,1730223.0,1999717.0,1000000.0]
-samplesrootfiles=["FreyasNtuple/DYJetsToLL_M-50_13TeV-madgraph-pythia8*.root",
-                  "FreyasNtuple/TTJets_MSDecaysCKM_central_Tune4C_13TeV-madgraph-tauola*.root",
-                  "FreyasNtuple/WplusToMuNu_CT10_13TeV-powheg-pythia8*.root",
-                  "FreyasNtuple/WminusToMuNu_CT10_13TeV-powheg-pythia8*.root",
-                  "FreyasNtuple/QCD_Pt-20to30_EMEnriched_Tune4C_13TeV_pythia8*.root",
-                  "FreyasNtuple/QCD_Pt-20toInf_MuEnrichedPt15_PionKaonDecay_Tune4C_13TeV_pythia8*.root",
-                  "FreyasNtuple/QCD_Pt-30to50_MuEnrichedPt5_PionKaonDecay_Tune4C_13TeV_pythia8*.root",
-                  "FreyasNtuple/QCD_Pt-50to80_MuEnrichedPt5_PionKaonDecay_Tune4C_13TeV_pythia8*.root",
-                  "FreyasNtuple/QCD_Pt-80to120_MuEnrichedPt5_PionKaonDecay_Tune4C_13TeV_pythia8*.root",
-                  "FreyasNtuple/QCD_Pt-80to170_EMEnriched_Tune4C_13TeV_pythia8*.root",
-                  "FreyasNtuple/QCD_Pt_170toInf_bcToE_Tune4C_13TeV_pythia8*.root",
-                  "FreyasNtuple/QCD_Pt_30to80_bcToE_Tune4C_13TeV_pythia8*.root",
-                  "FreyasNtuple/QCD_Pt_80to170_bcToE_Tune4C_13TeV_pythia8*.root"]
+
+pathToRootFile="/user/qpython/TopBrussels7X/CMSSW_7_2_1_patch1/src/TopBrussels/HToZZBachelorProjectNtupleMaker/FreyasNtuple"
+samplesrootfiles=[pathToRootFile+"/DYJetsToLL_M-50_13TeV-madgraph-pythia8*.root",
+                  pathToRootFile+"/TTJets_MSDecaysCKM_central_Tune4C_13TeV-madgraph-tauola*.root",
+                  pathToRootFile+"/WplusToMuNu_CT10_13TeV-powheg-pythia8*.root",
+                  pathToRootFile+"/WminusToMuNu_CT10_13TeV-powheg-pythia8*.root",
+                  pathToRootFile+"/QCD_Pt-20to30_EMEnriched_Tune4C_13TeV_pythia8*.root",
+                  pathToRootFile+"/QCD_Pt-20toInf_MuEnrichedPt15_PionKaonDecay_Tune4C_13TeV_pythia8*.root",
+                  pathToRootFile+"/QCD_Pt-30to50_MuEnrichedPt5_PionKaonDecay_Tune4C_13TeV_pythia8*.root",
+                  pathToRootFile+"/QCD_Pt-50to80_MuEnrichedPt5_PionKaonDecay_Tune4C_13TeV_pythia8*.root",
+                  pathToRootFile+"/QCD_Pt-80to120_MuEnrichedPt5_PionKaonDecay_Tune4C_13TeV_pythia8*.root",
+                  pathToRootFile+"/QCD_Pt-80to170_EMEnriched_Tune4C_13TeV_pythia8*.root",
+                  pathToRootFile+"/QCD_Pt_170toInf_bcToE_Tune4C_13TeV_pythia8*.root",
+                  pathToRootFile+"/QCD_Pt_30to80_bcToE_Tune4C_13TeV_pythia8*.root",
+                  pathToRootFile+"/QCD_Pt_80to170_bcToE_Tune4C_13TeV_pythia8*.root"]
 samplescolors=[4,2,rt.kGreen,rt.kGreen,rt.kYellow,rt.kYellow,rt.kYellow,rt.kYellow,rt.kYellow,rt.kYellow,rt.kYellow,rt.kYellow,rt.kYellow,rt.kYellow]
 
 stackplot_mu = rt.THStack("stackplot_mu",h_mud0.GetTitle())
