c0f88af050d170833f1b56052ce6e0d519f8b644
quentin <quentin.python@vub.ac.be>
2015-09-29 15:15:39 +0200

    removes unused variables and adds useful comments

--- TreeProcesser.cc
+++ TreeProcesser.cc
@@ -8,6 +8,7 @@
 #include <string>
 #include "TRandom3.h"
 #include "TNtuple.h"
+#include <sstream>
 
 //user code
 #include "TopTreeProducer/interface/TRootRun.h"
@@ -18,7 +19,7 @@
 #include "TopTreeAnalysisBase/Tools/interface/MultiSamplePlot.h"
 //#include "../macros/Style.C"
 
-#include <sstream>
+
 
 using namespace std;
 using namespace TopTree;
@@ -50,62 +51,37 @@ int main()
     bool DileptonElEl = false;
     bool SingleMu = true;
     bool SingleEl = false;
-    bool jetSplit = false;
-    bool jetTagsplit = true;
-
-    
-    //    string VoI = "BDT"; //variable of interest for plotting
-    //    string VoI = "nElectrons"; //variable of interest for plotting
     float lBound = 0;   //-1->0.2 topness
     float uBound = 3;
     int lumiScale = 50;  //Amount of luminosity to scale to in fb^-1
-
-    /*
-    vector<string> vars;
-    vars.push_back("HT");
-    vars.push_back("LeadingMuonPt");
-    vars.push_back("LeadingElectronPt");
-    vars.push_back("LeadingBJetPt");
-    vars.push_back("HT2M");
-    vars.push_back("MVAvals1");
-    */
-
     string xmlFileName;
     string xmlFileNameSys;
     string CraneenPath;
-    string splitVar, splitVar1, splitVar2;
     string splitting;
 
-    float bSplit, tSplit, wSplit, bSplit1, tSplit1, wSplit1, bSplit2, tSplit2, wSplit2;  //the bottom, top, and width of the splitting
 
-    if(SingleMu)
+    if(1)
     {
-
         xmlFileName = "config/Run2SingleLepton_samples.xml";
-	//	CraneenPath = "/user/lbeck/ThirteenTeV/CMSSW_7_2_1_patch1/src/TopBrussels/FourTop/Craneens_Mu/Craneens24_3_2015_merge/Craneen_";// faco here we set the craneen path
 	CraneenPath = "/user/qpython/TopBrussels7X/CMSSW_7_5_3/src/TopBrussels/DisplacedTops/MACRO_Output_MuEl/";
     }
 
     std::string slumiScale = intToStr(lumiScale);
 
-    if(jetSplit == true) splitting = "JS";
-    else if (jetTagsplit == true)  splitting = "JTS";
-    else{splitting = "inc";}
-
-
-
 
     cout << "CraneenPath is " << CraneenPath << endl;
     cout << "xmlFileName is " << xmlFileName << endl;
 
+
+
+    // calling datasetPlotter to create MSPplots
     DatasetPlotter(10, 0, 10, "nElectrons", xmlFileName,CraneenPath);
     DatasetPlotter(100, 0, 1000, "pt_electron[nElectrons]", xmlFileName,CraneenPath);
     DatasetPlotter(100, 0, 1000, "pt_muon[nMuons]", xmlFileName,CraneenPath);
     DatasetPlotter(100, -0.1, 0.1, "d0_muon[nMuons]", xmlFileName,CraneenPath);
 
+    // calling the function that writtes all the MSPlots in a root file
     MSPCreator ();
-
-
 
 }
 
@@ -117,9 +93,11 @@ void DatasetPlotter(int nBins, float plotLow, float plotHigh, string sVarofinter
   cout<<"RUNNING NOMINAL DATASETS"<<endl;
   cout<<""<<endl;
 
+  /*
   // vector with size equals to the number of time we call the DatasetPlotter function
   vector<string> vect_of_plot(4);
   vect_of_plot = {"nElectrons", "pt_electron[nElectrons]", "pt_muon[nMuons]", "d0_muon[nMuons]"};  
+  */
   
   const char *xmlfile = xmlNom.c_str();
   cout << "used config file: " << xmlfile << endl;
@@ -135,8 +113,8 @@ void DatasetPlotter(int nBins, float plotLow, float plotHigh, string sVarofinter
   treeLoader.LoadDatasets (datasets, xmlfile);	//cout<<"datasets loaded"<<endl;
   
     //***************************************************CREATING PLOTS****************************************************
-  TFile *outfile = new TFile((pathPNG+"/Output.root").c_str(),"recreate");
-  outfile->cd();
+  //  TFile *outfile = new TFile((pathPNG+"/Output.root").c_str(),"recreate");
+  //  outfile->cd();
   string plotname = sVarofinterest;   ///// Non Jet Split plot
   // make for loop here!!!
     MSPlot[plotname.c_str()] = new MultiSamplePlot(datasets, plotname.c_str(), nBins, plotLow, plotHigh, sVarofinterest.c_str()); 
@@ -148,14 +126,8 @@ void DatasetPlotter(int nBins, float plotLow, float plotHigh, string sVarofinter
   float ScaleFactor, NormFactor, Luminosity;
   int varofInterest;
   double varofInterest_double [20];
-  
-  string varofinterest_iterator;
-  string varofinterest_fixed;
-    
-  
-  
-  //    string sdelim = " []";
-  //    char* delim =  " []";
+
+
   
   vector<string> v;
   // to avoid modifying original string
@@ -172,35 +144,29 @@ void DatasetPlotter(int nBins, float plotLow, float plotHigh, string sVarofinter
   }
   free(dup);
 
+
   cout << v[0] << "  " << v[1] << endl;
   
 
   string CraneenPath = "/user/qpython/TopBrussels7X/CMSSW_7_5_3/src/TopBrussels/DisplacedTops/MACRO_Output_MuEl/";
 
-    //    cout << varofinterest_fixed << " " << varofinterest_iterator << endl;
   
   for (int d = 0; d < datasets.size(); d++)   //Loop through datasets  
     {
       dataSetName = datasets[d]->Name();
       cout<<"Dataset:  :"<<dataSetName<<endl;
-      
-      //        filepath = CraneenPath+dataSetName + "_Run2_TopTree_Study.root";
       filepath = CraneenPath+dataSetName + ".root";
-      //      filepath = dataSetName + ".root";//faco in
-      //cout<<"filepath: "<<filepath<<endl;
+      if (debug) cout<<"filepath: "<<filepath<<endl;
 	
-      FileObj[dataSetName.c_str()] = new TFile((filepath).c_str(),"READ"); //create TFile for each dataset
-      
-      //        string nTuplename = "Craneen__"+ leptoAbbr;
-      //        nTuple[dataSetName.c_str()] = (TNtuple*)FileObj[dataSetName.c_str()]->Get(nTuplename.c_str()); //get ntuple for each dataset
-      //        nEntries = (int)nTuple[dataSetName.c_str()]->GetEntries();
-      
+
+      FileObj[dataSetName.c_str()] = new TFile((filepath).c_str(),"READ"); //create TFile for each dataset      
       string TTreename = "tree";	
       ttree[dataSetName.c_str()] = (TTree*)FileObj[dataSetName.c_str()]->Get(TTreename.c_str()); //get ttre for each dataset
       nEntries = (int)ttree[dataSetName.c_str()]->GetEntries();
       cout<<"                 nEntries: "<<nEntries<<endl;
       
       
+      // bo logic to set the right branch address depending on the string given as argument of the datasetplotter
       if (v.size() == 2){
 	ttree[dataSetName.c_str()]->SetBranchAddress(v[1].c_str(),&varofInterest); 
 	ttree[dataSetName.c_str()]->SetBranchAddress(v[0].c_str(),varofInterest_double);
@@ -211,27 +177,27 @@ void DatasetPlotter(int nBins, float plotLow, float plotHigh, string sVarofinter
       }
       else {
 	cout << "Vector of string does not have the good size!!!" << endl;
-	}
-	     
-      //        ttree[dataSetName.c_str()]->SetBranchAddress(sVarofinterest_double.c_str(),&varofInterest_double);
-      //	ttree[dataSetName.c_str()]->SetBranchAddress("nElectrons",&nElectrons);
+      }
+      // eo logic to set the right branch address depending on the string given as argument of the datasetplotter
       
+      
+
       bool isData= false;
       if(dataSetName.find("Data")!=string::npos || dataSetName.find("data")!=string::npos || dataSetName.find("DATA")!=string::npos) isData =true;
       
-
       ScaleFactor = 1;
       Luminosity =1;
       
-      //for fixed bin width
       histo1D[dataSetName.c_str()] = new TH1F((dataSetName+"_"+v[0]).c_str(),(dataSetName+"_"+v[0]).c_str(), nBins, plotLow, plotHigh);
-      /////*****loop through entries and fill plots*****
+
+      // bo of loop through entries and fill plots
       for (int j = 0; j<nEntries; j++)
         {
 	  ttree[dataSetName.c_str()]->GetEntry(j);
-	  //artificial Lumi
+
 	  //	    cout << "Var of interest is " << varofInterest << endl;
 	  
+	  // bo of loop over the number of object per entry
 	  for (int i_object =0 ; i_object < varofInterest ;i_object ++ )
 	    {
 	      if (debug) cout << "varofInterest is " << varofInterest << endl;
@@ -257,7 +223,7 @@ void DatasetPlotter(int nBins, float plotLow, float plotHigh, string sVarofinter
       
       histo1D[dataSetName.c_str()]->Draw();
       string writename = "";
-      if(dataSetName.find("Data")!=string::npos || dataSetName.find("data")!=string::npos || dataSetName.find("DATA")!=string::npos)
+      if(isData)
         {
 	  writename = "data_nominal";
         }
@@ -307,7 +273,7 @@ void MSPCreator (){
 }
 
 
-
+// function that converts an int into a string
 std::string intToStr (int number){
   std::ostringstream buff;
   buff<<number;
