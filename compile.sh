8949e8ece0f362b20c0850887bad5e92ae6f5524
quentin <quentin.python@vub.ac.be>
2015-10-07 14:34:38 +0200

    allows to compile all .cc or only the one given in argument

--- compile.sh
+++ compile.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
-for ccfile in ./*.cc
-do
+# if there is one arg compile only this .cc
+if [[ -n $1 ]]
+then
+    ccfile=$1
+
     ofile=`echo $ccfile |sed 's/\.cc$//g'`
     echo "compiling : " $ccfile ", executible name: " $ofile
     g++ -g -std=c++11 -L ~/lib -L . -L .. -I ./ -I ../ -l TopTreeAnaContent76 -l TopTreeAna76 -l MLP -l TreePlayer -l TMVA -l XMLIO -I `root-config --incdir` `root-config --libs` $ccfile -o $ofile
-done
+
+# if there is no arg compile all .cc
+else
+    for ccfile in ./*.cc
+    do
+	ofile=`echo $ccfile |sed 's/\.cc$//g'`
+	echo "compiling : " $ccfile ", executible name: " $ofile
+	g++ -g -std=c++11 -L ~/lib -L . -L .. -I ./ -I ../ -l TopTreeAnaContent76 -l TopTreeAna76 -l MLP -l TreePlayer -l TMVA -l XMLIO -I `root-config --incdir` `root-config --libs` $ccfile -o $ofile
+    done
+fi
