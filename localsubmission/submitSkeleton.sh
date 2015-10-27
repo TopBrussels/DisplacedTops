873c4da9a51306a2db883c4284b4c5c4f9fc763d
quentin <quentin.python@vub.ac.be>
2015-10-27 11:42:21 +0100

    first commit of the trunc of all the submit scripts

---
+++ localsubmission/submitSkeleton.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#PBS -q localgrid
+#PBS -l walltime=02:00:00
+
+# setting up your code and your env
+cd /user/qpython/TopBrussels7X/CMSSW_7_4_14/src/TopBrussels/DisplacedTops
+eval `scramv1 runtime -sh`
+
+# want you really want to do!!
+
+
