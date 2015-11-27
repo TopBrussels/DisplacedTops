30842f0ccc8800eed5cc3702f824743ef452d746
quentin <quentin.python@vub.ac.be>
2015-11-27 11:24:40 +0100

    reduces wall time to 30. This could be optimised further.

--- localsubmission/submitSkeleton.sh
+++ localsubmission/submitSkeleton.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #PBS -q localgrid
-#PBS -l walltime=01:00:00
+#PBS -l walltime=0:30:00
 
 # setting up your code and your env
 cd /user/qpython/TopBrussels7X/CMSSW_7_4_14/src/TopBrussels/DisplacedTops
