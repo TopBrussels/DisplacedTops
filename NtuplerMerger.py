2f8fa1f16a6355185920dc0901d4f25e94e6a15a
quentin <quentin.python@vub.ac.be>
2016-01-14 13:48:01 +0100

    adds feature to print out all the zombie files

--- NtuplerMerger.py
+++ NtuplerMerger.py
@@ -24,8 +24,8 @@ if not os.path.exists(pathMerged):
 
 
 # get filenames from the xml!!!
-tree = ET.ElementTree(file='config/FullMcBkgdSamplesV9.xml')
-#tree = ET.ElementTree(file='config/DataSamples.xml')
+#tree = ET.ElementTree(file='config/FullMcBkgdSamplesV9.xml')
+tree = ET.ElementTree(file='config/DataSamples.xml')
 
 root = tree.getroot()
 datasets = root.find('datasets')
@@ -36,23 +36,38 @@ datasetNames = []
 for d in datasets:
     if d.attrib['add'] == '1':
         print "found dataset to be added..." + str(d.attrib['name'])
-        if "SingleTop_tW" in str(d.attrib['name']) :
+        # select a subset of the existing root file
+        if "Dataasdfa" in str(d.attrib['name']) :
             datasetNames.append(str(d.attrib['name']))
             print str(d.attrib['name'])
 
 
+listOfZombie= []
 
 for n in datasetNames:
     filenames = glob.glob(pathNonMerged + "/*" + n + "*.root")
     hadd = "hadd " + pathMerged + "DisplacedTop_Run2_TopTree_Study_" + n + "_MuEl" + ".root"
     for f in filenames:
         file=ROOT.TFile(f,"read")
+        # check if the file is a zombie
         if (file.IsZombie()):
             print "File" , f, "is a Zombie.... Skipping"
+            listOfZombie.append(f)
         else:
-            print "faco"
             print f
             hadd = hadd + " " + f
     print "Merging ntuples for " + n
     os.system(hadd)
+
+print "\n\n"
+
+# print the list of zombies
+if (len(listOfZombie) > 0):
+    print "The total number of zombie file is ", len(listOfZombie)
+    print "And the list of the zombie is :"
+    for zombie in listOfZombie:
+        print zombie
+
+
+
 
