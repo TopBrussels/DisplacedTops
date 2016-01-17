de1cbeb87cf2255617a70cbefc483f2fdc040e83
quentin <quentin.python@vub.ac.be>
2016-01-17 18:52:02 +0100

    loop over the channels and take the corresponding xml file. If there is zombie files they are put in a file under the target directory

--- NtuplerMerger.py
+++ NtuplerMerger.py
@@ -13,37 +13,46 @@ mm = str(now.month)
 yyyy = str(now.year)
 # pick one of the two above
 date = dd+"_"+mm+"_"+yyyy
-
-
 #date = "1_12_2015"
-#channel = ["_MuEl","_MuMu","_ElEl"]
-channel = ["_MuMu"]
 
+channels = ["_MuMu","_ElEl"]
 
-for chan in channel:
+for chan in channels:
     
     #Define path where ntuples are stored
-    pathNonMerged = "MACRO_Output"+chan+"/"  #needs to be changed for different lepton channel
+    pathNonMerged = "MACRO_Output"+chan+"/"  
     pathMerged = "MergedTrees/"+date+"/"+chan+"/"
     
     if not os.path.exists(pathMerged):
         os.makedirs(pathMerged)
     
-    
-    # get filenames from the xml!!!
+    # get filenames from the xml!!!    
+    if "MuMu" in chan:
+        tree = ET.ElementTree(file='config/FullSamplesMuMuV9.xml')
+    elif "ElEl" in chan:
+        tree = ET.ElementTree(file='config/FullSamplesElElV9.xml')
+    elif "ElMu" in chan:
+        tree = ET.ElementTree(file='config/FullSamplesElMuV9.xml')
+    else:
+        print "No tree has been loaded!!! Make sure the correct xml file are in the right directories!!!"
+        sys.exit()
+
     #tree = ET.ElementTree(file='config/FullMcBkgdSamplesV9.xml')
-    tree = ET.ElementTree(file='config/DisplacedTopsSignal.xml')
+    #tree = ET.ElementTree(file='config/DisplacedTopsSignal.xml')
     #tree = ET.ElementTree(file='config/DataSamples.xml')
-    
+
+    # get the list of dataset
     root = tree.getroot()
     datasets = root.find('datasets')
     print "found  "  + str(len(datasets)) + " datasets"
     datasetNames = []
     
+    print ""
     # loop over the datasets to be added and fill the "topTrees" vector
     for d in datasets:
         if d.attrib['add'] == '1':
             print "found dataset to be added..." + str(d.attrib['name'])
+
             # select a subset of the existing root file
             if "" in str(d.attrib['name']) :
                 datasetNames.append(str(d.attrib['name']))
@@ -52,31 +61,38 @@ for chan in channel:
     
     listOfZombie= []
     
+    # loop over data set to search root files
     for n in datasetNames:
         filenames = glob.glob(pathNonMerged + "/*" + n + chan + "*.root")
         hadd = "hadd " + pathMerged + "DisplacedTop_Run2_TopTree_Study_" + n + chan + ".root"
-        for f in filenames:
-            file=ROOT.TFile(f,"read")
-            # check if the file is a zombie
-            if (file.IsZombie()):
-                print "File" , f, "is a Zombie.... Skipping"
-                listOfZombie.append(f)
-            else:
-                print f
-                hadd = hadd + " " + f
-        print "Merging ntuples for " + n
-        os.system(hadd)
-    
+
+        if (len(filenames) == 0):
+            print "no root files found in directory" , pathNonMerged ,  " for dataset " , n , " !!"
+        else :
+            # loop over root files
+            for f in filenames:
+                file=ROOT.TFile(f,"read")
+                # check if the file is a zombie
+                if (file.IsZombie()):
+                    print "File" , f, "is a Zombie.... Skipping"
+                    listOfZombie.append(f)
+                else:
+                    print f
+                    hadd = hadd + " " + f
+            print "Merging ntuples for " + n
+            os.system(hadd)
+        
     print "\n\n"
     
     # print the list of zombies
+    print "The total number of zombie file is ", len(listOfZombie)
     if (len(listOfZombie) > 0):
-        print "The total number of zombie file is ", len(listOfZombie)
-        print "And the list of the zombie is :"
+        outfile = open (pathMerged+"/Zombie"+chan+".txt", 'a')
+        print "And the list of the zombie is put in "+pathMerged+"/Zombie"+chan+".txt "
         for zombie in listOfZombie:
-            print zombie
+            print >> outfile, zombie
     
-    mergeData=False
+    mergeData=True
     
     if (mergeData):
     # combining all the Data in one
