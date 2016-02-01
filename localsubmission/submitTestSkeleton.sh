22e0db6007bfca69c0b0c780b21c8d380b37882a
quentin <quentin.python@vub.ac.be>
2016-02-01 13:25:30 +0100

    first commit of the a skeleton that allows runing test job on the express queue

---
+++ localsubmission/submitTestSkeleton.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#PBS -q express
+
+# setting up your code and your env
+cd /user/qpython/TopBrussels7X/CMSSW_7_4_14/src/TopBrussels/DisplacedTops
+eval `scramv1 runtime -sh`
+
+# want you really want to do!!
+
+
