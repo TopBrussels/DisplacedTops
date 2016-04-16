c58ff725cd4aa2a99d9279efec166f090a7325aa
quentin <quentin.python@vub.ac.be>
2016-04-16 16:34:58 +0200

    makes a loop over composite dataset. Various clean up and addition of few comments

--- scripts/create2DHistoForFactorization.py
+++ scripts/create2DHistoForFactorization.py
@@ -1,87 +1,95 @@
 ############
-# Pyroot macro to create d0 histograms from bb+El and bb+Mu Control region. These histograms will be used in the ClosureTestQCDEstimate.py script in order to caclculate Transfer Factors (TFs) to make the closure test.
+#Pyroot script that reads some tree and fill d0 vs d0 2D histograms. 
+#These 2D histograms are used to extend the non-zero prediction of the given background in the SR using the product of the efficiency to pass the d0 cut of the first lepton with the efficiency to pass the d0 cut of the second lepton
 # April 2016 by qpython@cern.ch 
 
+# basic import
 import xml.etree.cElementTree as ET
 import os, sys
 from array import array
 import ROOT as rt
 
 
-#channel                                                                                                                       
-#channels=["_MuMu"]                                                                                                            
-channels=["_ElEl"]                                                                                                            
-#channels=["_ElEl","_MuMu"]
+#channel 
+#channels=["_MuMu"] 
+#channels=["_ElEl"] 
+channels=["_ElEl","_MuMu"]
 
 
 #root file  
-#radix of the path
+#base of the path to the root file
 pathTrunc="/user/qpython/TopBrussels7X/CMSSW_7_6_3/src/TopBrussels/DisplacedTops/MergedTrees/"
 #date
 date="15_4_2016"
 
+# array with composite dataset and matching string
+dataSetTitles=["WJets", "Diboson", "SingleTop", "TTJets", "ZToll"]
+compositeDatasets= ["rightarrow l+", "Diboson", "SingletTop", "TTJets_Madgraph", "gamma^{*}"]
 
-# output root file
-outfile = rt.TFile("rootFiles/2D.root",'RECREATE')
 
-# bool
+# verbosity
 debug = False
 
-# lumi
-lumivalue = 1
-weight = 1
 
+# FACO NEED TO BE DECLARED INSIDE THE LOOP WIth name inside the histo
 # define do histograms
 electrond0VsElectronsd0=rt.TH2D("electrond0VsElectronsd0","electrond0VsElectronsd0", 50, 0.0, 0.05, 50, 0.0, 0.05)
 muond0VsMuond0=rt.TH2D("muond0VsMuond0","muond0VsMuond0",50, 0.0, 0.05, 50, 0.0, 0.05)
 muond0VsElectrond0=rt.TH2D("muond0VsElectrond0","muond0VsElectrond0",50, 0.0, 0.05, 50, 0.0, 0.05)
 
 
-
-# to be fixed if you want to run on MC instead of Data
-
-#tree = ET.ElementTree(file='../config/Yield_FullSamplesMuMuV0.xml')
-#treeName="doubleMuTree"
-
-tree = ET.ElementTree(file='../config/Yield_FullSamplesElElV0.xml')
-treeName="doubleElTree"
-
+# name of the tree in the root file
 treeName="tree"
 
+# loop over the different composite data set
+i_comp=0
+for compositeDataset in compositeDatasets:
+    print "\n", "compositeDataset is " , dataSetTitles[i_comp] , ":"
+          
+    FilterString=compositeDatasets[i_comp]
+    outfile = rt.TFile("rootFiles/"+dataSetTitles[i_comp]+"2D.root",'RECREATE')
 
-
-for chan in channels :
-    isElEl =False
-    isMuMu =False
-
-
-    if chan == "_ElEl" :
-        isElEl = True
-    if chan == "_MuMu" :
-        isMuMu = True
+    # loop over the different channels
+    for chan in channels :
+        isElEl =False
+        isMuMu =False
     
     
-    root =  tree.getroot()
-    datasets = root.find('datasets')
-    print "found  "  + str(len(datasets)) + " datasets"
-    datasetNames = []
+        if chan == "_ElEl" :
+            isElEl = True
+        if chan == "_MuMu" :
+            isMuMu = True
     
-    # loop over the composite datasets (title)
-    for i in range(0,1): # to be removed!!
+        if isElEl:
+            tree = ET.ElementTree(file='../config/Yield_FullSamplesElElV0.xml')    
+        if isMuMu:
+            tree = ET.ElementTree(file='../config/Yield_FullSamplesMuMuV0.xml')
+
+
+        root =  tree.getroot()
+        datasets = root.find('datasets')
+        if (debug):
+            print "found  "  + str(len(datasets)) + " datasets"
         
+        # reset the list of dataset
+        datasetNames = []
+
 
         # loop over the dataset inside the composite dataset (name)
+        i_dataset=0
         for d in datasets:
 
-            # only for Data
-            if d.attrib['add'] == '1': 
-#            if d.attrib['add'] == '1' and compositeDataset == str(d.attrib['title']): 
+            # get the lumi from the Data
+            if d.attrib['add'] == '1' and "Data" in str(d.attrib['name']):
+                lumivalue=float(d.attrib['EqLumi'])
+
+            # check if in the right composite dataset
+            if d.attrib['add'] == '1' and FilterString in  str(d.attrib['title']): 
                 datasetNames.append(str(d.attrib['name']))
                 print str(d.attrib['name'])
                 ch = rt.TChain(treeName,treeName)
                 sampleName=d.attrib['name']
 
-#                outfile = rt.TFile("rootFiles/"+sampleName+"2D.root",'RECREATE')
 
                 # fix the type of dataset (bgMC, signal or data)                           
                 isBgMC = False
@@ -94,35 +102,32 @@ for chan in channels :
                     isSignal = True
                 if "Data" in sampleName:
                     isData = True
+                
+
+                ch.Add(pathTrunc+date+"/"+chan+"/DisplacedTop_Run2_TopTree_Study_"+sampleName+chan+".root")
 
         
-                ch.Add(pathTrunc+date+"/"+chan+"/DisplacedTop_Run2_TopTree_Study_"+sampleName+chan+".root")
-    
-        
-                nevents=ch.GetEntries()
-        
-        
-                if isData :
-                    lumivalue=float(d.attrib['EqLumi'])
-                        
-                    weight= lumivalue / float(d.attrib['EqLumi'])
-                if (1):
+                # get the lumi weight 
+                weight= lumivalue / float(d.attrib['EqLumi'])
+                if (debug):
                     print "lumivalue is " ,lumivalue
                     print " float(d.attrib['EqLumi']) is ",  float(d.attrib['EqLumi'])
                     print "weight is " , weight
-        
+
+
+                # start of loop over events 
                 ii=0
-                # start of loop over events                                                                                                                
                 for iev in ch:
     
+                    # PU weight
                     if isMuMu:
                         PileUpWeight=iev.evt_puSF
                     if isElEl:
                         PileUpWeight=iev.evt_puSF
         
-                    
+
+                    # Lepton Weight
                     LeptonWeight=1.0
-                    
                     for ilept in range (0,2):
                         if isMuMu:
                             LeptonWeight *= iev.sf_muon[0]
@@ -130,8 +135,7 @@ for chan in channels :
                             LeptonWeight *= iev.sf_electron[0]
                     
         
-        
-                    # define shorter variable                                                                                                              
+                    # Filling corresponding histograms
                     if isMuMu:
                         d01mu=abs(iev.d0BeamSpot_muon[0])
                         d02mu=abs(iev.d0BeamSpot_muon[1])
@@ -144,17 +148,24 @@ for chan in channels :
                     if isElEl :
                         d01el=abs(iev.d0BeamSpot_electron[0])
                         d02el=abs(iev.d0BeamSpot_electron[1])
-#                        electrond0VsElectronsd0.Fill(d01el,d02el,weight*PileUpWeight*LeptonWeight)
-                        electrond0VsElectronsd0.Fill(d01el,d02el)
+                        electrond0VsElectronsd0.Fill(d01el,d02el,weight*PileUpWeight*LeptonWeight)
         
+                    # eo loop over the vent
 
-    
+            # eo loop over the dataset
 
-# end of loop
+        #eo loop over the channnel
+
+    # write the two histo and close the file
+    outfile.cd()
+    muond0VsMuond0.Write()
+    electrond0VsElectronsd0.Write()
+    outfile.Close()
+    i_comp=i_comp+1
 
 
-# write the histo in the output file
-outfile.cd()
-muond0VsMuond0.Write()
-electrond0VsElectronsd0.Write()
-outfile.Close()
+
+    # end of loop over the comp dataset
+
+
+
