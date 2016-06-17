8dd31e7328879e6049bdf13b670a6eb3ee5cf268
quentin <quentin.python@vub.ac.be>
2016-06-17 12:32:21 +0200

    adds legend. Still quite some things to be fixed though..

--- scripts/createMCVsSignalMonneyPlot.py
+++ scripts/createMCVsSignalMonneyPlot.py
@@ -11,6 +11,7 @@ plot with the Sum of the background vs a signal sample in the 2D d0 plane.
 
 #basic import
 from array import array
+import array
 from ROOT import *
 import ROOT as rt
 import CMS_lumi, tdrstyle
@@ -136,6 +137,79 @@ for chan in channels:
     frame = canvas.GetFrame()
     frame.Draw()
 
+
+    #set the colors and size for the legend
+    histLineColor = rt.kOrange+7
+    histFillColor = rt.kOrange-2
+    markerSize  = 1.0
+    
+    latex = rt.TLatex()
+    n_ = 2
+    
+    x1_l = 0.92
+    y1_l = 0.60
+
+    dx_l = 0.30
+    dy_l = 0.18
+    x0_l = x1_l-dx_l
+    y0_l = y1_l-dy_l
+    
+    legend =  rt.TPad("legend_0","legend_0",x0_l,y0_l,x1_l, y1_l )
+    #legend.SetFillColor( rt.kGray )
+    legend.Draw()
+    legend.cd()
+
+    ar_l = dy_l/dx_l
+    #gap_ = 0.09/ar_l
+    gap_ = 1./(n_+1)
+    bwx_ = 0.12
+    bwy_ = gap_/1.5
+
+    x_l = [1.2*bwx_]
+    #y_l = [1-(1-0.10)/ar_l]
+    y_l = [1-gap_]
+    ex_l = [0]
+    ey_l = [0.04/ar_l]
+    
+   #array must be converted 
+    x_l = array.array("f",x_l)
+    ex_l = array.array("f",ex_l)
+    y_l = array.array("f",y_l)
+    ey_l = array.array("f",ey_l)
+    
+    gr_l =  rt.TGraphErrors(1, x_l, y_l, ex_l, ey_l)
+    
+    rt.gStyle.SetEndErrorSize(0)
+    gr_l.SetMarkerSize(0.9)
+    gr_l.Draw("0P")
+    
+    latex.SetTextFont(42)
+    latex.SetTextAngle(0)
+    latex.SetTextColor(rt.kBlack)    
+    latex.SetTextSize(0.25)    
+    latex.SetTextAlign(12) 
+    
+#    box_ = rt.TBox()
+    xx_ = x_l[0]
+    yy_ = y_l[0]
+    latex.DrawLatex(xx_+1.*bwx_,yy_,"RPV stops, c#tau 1 cm")
+
+    yy_ -= gap_
+#    box_.SetLineStyle( rt.kSolid )
+#    box_.SetLineWidth( 1 )
+    # box_.SetLineColor( kBlack )
+#    box_.SetLineColor( histLineColor )
+#    box_.SetFillColor( histFillColor )
+#    box_.DrawBox( xx_-bwx_/2, yy_-bwy_/2, xx_+bwx_/2, yy_+bwy_/2 )
+#    box_.SetFillStyle(0)
+#    box_.DrawBox( xx_-bwx_/2, yy_-bwy_/2, xx_+bwx_/2, yy_+bwy_/2 )
+    #Draw Z->ee text
+    latex.DrawLatex(xx_+1.*bwx_,yy_,"SM background ")
+
+    #update the canvas to draw the legend
+    canvas.Update()
+
+    raw_input("Press Enter to end")
 
     canvas.Print("plots/MonneyPlot"+chan+".pdf")
 # eo loop over the channels
