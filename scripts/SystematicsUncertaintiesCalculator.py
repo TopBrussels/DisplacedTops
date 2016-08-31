b0ace34af48c3320e12481ee59672e7cd997ebfb
quentin <quentin.python@vub.ac.be>
2016-08-31 09:02:27 +0200

    adds a cd to loop over the json files in the jsonFiles folder. Adds a dictionary with the cst systematics and its value (to be checked).

--- scripts/SystematicsUncertaintiesCalculator.py
+++ scripts/SystematicsUncertaintiesCalculator.py
@@ -4,8 +4,7 @@ The script works as follows:
 1) make a list of all the .json files in the current directory
 2) create a list of dict where each element corresponds to the content of one json file
 3) merge everything in a single dict and save it to a new json file
-
-
+4) use the merged dictionary to produce the systematics uncertainty table with the total error.
 
 qpython 30.08.2016
 """
@@ -16,10 +15,12 @@ import os
 from tabulate import tabulate
 
 # json file list
+os.chdir("jsonFiles")
 jsonFiles=[]
 for fn in os.listdir('.'):
-     if os.path.isfile(fn) and ".json" in fn:
-         jsonFiles.append(fn)
+     print fn
+     if ".json" in fn:
+          jsonFiles.append(fn)
 
 print "the list of json files to be use is \n", jsonFiles
 
@@ -48,16 +49,21 @@ print "the merged dict is \n"
 print Yield_dict
 
 
+dict_cstSystType={"lumi":0.05, "trigger_sf_electron":0.20, "trigger_sf_muon":0.10, "trk_sf_electron":0.24, "trk_sf_muon":0.12 }
 
 # 
-systTypes=["sf_reco_electron","sf_id_electron","evt_puSF","sf_iso_muon","sf_id_muon","Central"]
+#MuonSystTypes=["Cross-Section","evt_Pusf","Sf_Iso_Muon","sf_id_muon","lumi","trigger_sf_muon", "trk_sf_muon"]
+MuonSystTypes=["evt_puSF","sf_iso_muon","sf_id_muon","lumi","trigger_sf_muon", "trk_sf_muon"]
+electronSystTypes=["Cross-section","evt_puSF","sf_reco_electron","sf_id_electron","trigger_sf_electron", "trk_sf_electron"]
 sampleNames=["ZG","WWTo2l2Nu"]
 
-# double array for table writting                                                                                                                                                                                
+# double array for table writting 
 doubleArray = []
 singleArray = []
 headers=["Dataset"]
 
+
+systTypes=list(MuonSystTypes)
 
 # loop over the samples 
 for sampleName in sampleNames:
@@ -70,19 +76,25 @@ for sampleName in sampleNames:
         for systType in systTypes :
             headers.append(systType)
 
-            # one variable per yield
-            YieldDown= Yield_dict[sampleName+systType+"down"]
-            YieldUp= Yield_dict[sampleName+systType+"up"]
-            YieldCentral =  Yield_dict[sampleName+"Centralup"]
-            
-            # two diff wrt to central
-            diffDown=abs((YieldCentral-YieldDown)/YieldCentral)
-            diffUp=abs((YieldCentral-YieldUp)/YieldCentral)
 
-            # max of the two effs
-            uncertaintyMax=  max(diffDown,diffUp)
+            if systType in dict_cstSystType:
+                 print "faco!!!", systType
+                 uncertaintyMax = 0
+            else :
+                # one variable per yield
+                YieldDown= Yield_dict[sampleName+systType+"down"]
+                YieldUp= Yield_dict[sampleName+systType+"up"]
+                YieldCentral =  Yield_dict[sampleName+"Centralup"]
+                
+                # two diff wrt to central
+                diffDown=abs((YieldCentral-YieldDown)/YieldCentral)
+                diffUp=abs((YieldCentral-YieldUp)/YieldCentral)
+    
+                # max of the two effs
+                uncertaintyMax=  max(diffDown,diffUp)
+
             uncertaintyMaxPercent = str(100*uncertaintyMax)+" %"
-
+    
             # Sum the square of the uncertainties
             CurrentUnc=uncertaintyMax
             SumUnc=SumUnc+CurrentUnc*CurrentUnc
