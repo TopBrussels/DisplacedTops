d4a415fe2ea98ce3fabc51898802c229763264b0
quentin <quentin.python@vub.ac.be>
2016-11-29 14:34:05 +0100

    adds extra print out like the number of submited jobs

--- localsubmission/SubmitAll.sh
+++ localsubmission/SubmitAll.sh
@@ -1,19 +1,26 @@
 #!/bin/bash 
+
+iterator=0
+
 if [[ -n $1 ]] #check if variable is not empty
 then
     if [[ $1 == "test" ]]
     then
+	echo "Using test option! Only submiting one file per sample"
 	cd test
 	for f in ./submit*.sh
 	do
 	    qsub $f
+	    let "iterator++"
 	done
 	cd -
     else
+	echo "A filter is being used! The filter is" $1
 	cd output
         for f in ../submit_$1*.sh
         do
             qsub $f
+	    let "iterator++"
         done
         cd -
     fi
@@ -23,7 +30,10 @@ else
     for f in ../submit*.sh
     do
 	qsub $f
+	let "iterator++"
     done
     cd -
 
 fi
+
+echo "the total number of submited job is " $iterator
