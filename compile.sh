3c742a29aca0bf07d909ca3e7352a44ccb5c56bc
Freya Blekman <freya.blekman@cern.ch>
2017-10-09 17:29:21 +0200

    movving to 80X

--- compile.sh
+++ compile.sh
@@ -7,7 +7,7 @@ then
 
     ofile=`echo $ccfile |sed 's/\.cc$//g'`
     echo "compiling : " $ccfile ", executible name: " $ofile
-    g++ -g -std=c++11 -L ~/lib -L . -L .. -I ./ -I ../ -l TopTreeAnaContent76 -l TopTreeAna76 -l MLP -l TreePlayer -l TMVA -l XMLIO -I `root-config --incdir` `root-config --libs` $ccfile -o $ofile
+    g++ -g -std=c++11 -L ~/lib -L . -L .. -I ./ -I ../ -l TopTreeAnaContent80 -l TopTreeAna80 -l MLP -l TreePlayer -l TMVA -l XMLIO -I `root-config --incdir` `root-config --libs` $ccfile -o $ofile
 
 
 # if there is no arg compile all .cc
@@ -16,7 +16,7 @@ else
     do
 	ofile=`echo $ccfile |sed 's/\.cc$//g'`
 	echo "compiling : " $ccfile ", executible name: " $ofile
-	g++ -g -std=c++11 -L ~/lib -L . -L .. -I ./ -I ../ -l TopTreeAnaContent76 -l TopTreeAna76 -l MLP -l TreePlayer -l TMVA -l XMLIO -I `root-config --incdir` `root-config --libs` $ccfile -o $ofile
+	g++ -g -std=c++11 -L ~/lib -L . -L .. -I ./ -I ../ -l TopTreeAnaContent80 -l TopTreeAna80 -l MLP -l TreePlayer -l TMVA -l XMLIO -I `root-config --incdir` `root-config --libs` $ccfile -o $ofile
 
     done
 fi
